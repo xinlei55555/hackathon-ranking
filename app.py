8270e0e80be9bc3a5eb925580c037e4fbf0a4bbf
Aly <alyshariff2810@gmail.com>
2023-07-26 18:24:46 -0400

    CHat feature is done

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 from io import StringIO
 import pandas as pd
 st.title("HYPE AI HAckathon") # Title
-
+import time
+import random
 
 # '''
 # Our app should be able to:
@@ -17,16 +18,42 @@ st.title("HYPE AI HAckathon") # Title
 # Load messages from session storage
 if "messages" not in st.session_state:
     st.session_state.messages = []
-# Render each message in the chat container
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+
 uploaded_file = st.file_uploader("Upload a PDF")
 with st.chat_message("assistant"):
     st.write("Hello 👋")
-prompt = st.chat_input("Say something...")
+    # Render each message in the chat container
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-
+# Render new msg each time a new message is entered
+if prompt := st.chat_input("Say something..."): 
+# We used the := operator to assign the user's input to the prompt variable and checked if it's not None in the same line. If the user has sent a message, we display the message in the chat message container and append it to the chat history.
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Assistant response
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = random.choice(
+            [
+                "Hello there! How can I assist you today?",
+                "Hi, human! Is there anything I can help you with?",
+                "Do you need help?",
+            ]
+        )
+        # Simulate stream of response with milliseconds delay
+        for chunk in assistant_response.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            # Add a blinking cursor to simulate typing
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
 if uploaded_file is not None:
     # To read file as bytes:
     print("OK")
