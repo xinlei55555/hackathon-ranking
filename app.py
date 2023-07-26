41e1303adfbd9657ff50ee85e819e5d99529d901
Aly <alyshariff2810@gmail.com>
2023-07-26 18:14:17 -0400

    WIP

--- app.py
+++ app.py
@@ -14,9 +14,19 @@ st.title("HYPE AI HAckathon") # Title
 # Transcript
 # Summarizer
 # Chat functionality
-
-
+# Load messages from session storage
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+# Render each message in the chat container
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 uploaded_file = st.file_uploader("Upload a PDF")
+with st.chat_message("assistant"):
+    st.write("Hello 👋")
+prompt = st.chat_input("Say something...")
+
+
 if uploaded_file is not None:
     # To read file as bytes:
     print("OK")
@@ -30,3 +40,5 @@ if uploaded_file is not None:
     # To read file as string:
     string_data = stringio.read()
     st.write(string_data)
+
+
